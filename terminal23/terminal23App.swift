08514d5b351ff847cd6593cc42d4796c9d644028
Yevhenii Hyzyla <y.hyzyla@vchasno.ua>
2023-04-22 10:49:46 +0200

    Initial Commit

---
+++ terminal23/terminal23App.swift
@@ -0,0 +1,17 @@
+//
+//  terminal23App.swift
+//  terminal23
+//
+//  Created by Yevhenii Hyzyla on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct terminal23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
