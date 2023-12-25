14b8bce8f1d8ad5c327f1072ba888758e0b97b46
Yevhenii Hyzyla <y.hyzyla@vchasno.ua>
2023-12-25 18:51:49 +0100

    Init

---
+++ terminal23/BlogDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  BlogDetailView.swift
+//  terminal23
+//
+//  Created by Yevhenii Hyzyla on 22.04.2023.
+//
+
+import Foundation
---
+++ terminal23/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  terminal23
+//
+//  Created by Yevhenii Hyzyla on 22.04.2023.
+//
+
+import Foundation
